f86b004f146f770c0606b058b1eb520b3fe049b9
Ronan Dance <ronan.dance@virginmedia.com>
2026-02-13 00:47:01 +0000

    Added json saving functionality

--- video_stats.py
+++ video_stats.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from datetime import date
 from dotenv import load_dotenv
 
 
@@ -87,7 +88,14 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f"./data/yt-etl_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as josn_outfile:
+        json.dump(extracted_data, josn_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
