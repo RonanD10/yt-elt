59ff340342c6404eb1c681e636e4118d82d66771
Ronan Dance <ronan.dance@virginmedia.com>
2026-02-18 01:48:44 +0000

    Updated main to restructure DAGs

--- dags/main.py
+++ dags/main.py
@@ -35,14 +35,14 @@ default_args = {
 staging_schema = "staging"
 core_schema = "core"
 
-
+# DAG 1: produce_json
 with DAG(
-    dag_id ="produce_json",
-    default_args=default_args, 
-    description="DAG to produce JSON file with raw data", 
-    schedule="0 14 * * *", 
-    catchup=False
-) as dag: 
+    dag_id="produce_json",
+    default_args=default_args,
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
+    catchup=False,
+) as dag_produce:
 
     # Define tasks
     playlist_id = get_playlist_id()
@@ -50,33 +50,47 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
 
+    # Define dependencies
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
+# DAG 2: update_db
 with DAG(
     dag_id="update_db",
     default_args=default_args,
     description="DAG to process JSON file and insert data into both staging and core schemas",
     catchup=False,
-    schedule="0 15 * * *",
+    schedule=None,
 ) as dag_update:
 
     # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
-    update_staging >> update_core 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
+    # Define dependencies
+    update_staging >> update_core >> trigger_data_quality
 
+# DAG 3: data_quality
 with DAG(
     dag_id="data_quality",
     default_args=default_args,
-    description="DAG to check the data quality on both layers in the db.",
+    description="DAG to check the data quality on both layers in the database",
     catchup=False,
-    schedule="0 16 * * *",
-) as dag_update:
+    schedule=None,
+) as dag_quality:
 
+    # Define tasks
     soda_validate_staging = yt_elt_data_quality(staging_schema)
     soda_validate_core = yt_elt_data_quality(core_schema)
 
+    # Define dependencies
     soda_validate_staging >> soda_validate_core
